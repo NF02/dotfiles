e86b2f7fb804500885f2cf413858492ba7ff3420
Nicola Ferru Aka NFVblog <ask.nfvblog@outlook.it>
2024-06-19 13:10:48 +0200

    void linux setup
    void Linux + sway + alacritty + pipewire + emacs server + portal

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,57 +5,9 @@
 ;; theme
 (load-theme 'adwaita t)
 
-					; ido mode
-;; prev list
-(setq completion-auto-help 'visible)
-;; autocomp by selection
-(setq completion-auto-select 'second-tab)
+;; ido mode
+(ido-mode t)
 
-(defun renz/sort-by-alpha-length (elems)
-  "Sort ELEMS first alphabetically, then by length."
-  (sort elems (lambda (c1 c2)
-                (or (string-version-lessp c1 c2)
-                    (< (length c1) (length c2))))))
-
-(defun renz/sort-by-history (elems)
-  "Sort ELEMS by minibuffer history.
-Use `mct-sort-sort-by-alpha-length' if no history is available."
-  (if-let ((hist (and (not (eq minibuffer-history-variable t))
-                      (symbol-value minibuffer-history-variable))))
-      (minibuffer--sort-by-position hist elems)
-    (renz/sort-by-alpha-length elems)))
-
-(defun renz/completion-category ()
-  "Return completion category."
-  (when-let ((window (active-minibuffer-window)))
-    (with-current-buffer (window-buffer window)
-      (completion-metadata-get
-       (completion-metadata (buffer-substring-no-properties
-                             (minibuffer-prompt-end)
-                             (max (minibuffer-prompt-end) (point)))
-                            minibuffer-completion-table
-                            minibuffer-completion-predicate)
-       'category))))
-
-(defun renz/sort-multi-category (elems)
-  "Sort ELEMS per completion category."
-  (pcase (renz/completion-category)
-    ('nil elems) ; no sorting
-    ('kill-ring elems)
-    ('project-file (renz/sort-by-alpha-length elems))
-    (_ (renz/sort-by-history elems))))
-
-(setq completions-sort #'renz/sort-multi-category)
-
-;; Up/down when completing in the minibuffer
-(define-key minibuffer-local-map (kbd "C-p") #'minibuffer-previous-completion)
-(define-key minibuffer-local-map (kbd "C-n") #'minibuffer-next-completion)
-
-;; Up/down when competing in a normal buffer
-(define-key completion-in-region-mode-map (kbd "C-p") #'minibuffer-previous-completion)
-(define-key completion-in-region-mode-map (kbd "C-n") #'minibuffer-next-completion)
-
-					; end ido
 ;; ------------------------------------------------------------------
 ;; plugin
 ;; ------------------------------------------------------------------
