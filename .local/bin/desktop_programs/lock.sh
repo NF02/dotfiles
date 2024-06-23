b033add9f0c9628894426a817fd2af9645e0fe94
Nicola Ferru Aka NFVblog <ask.nfvblog@outlook.it>
2024-06-23 20:02:51 +0200

    Device menu
    Device menu is a tool for mounting and dismounting USB peripherals, SD
    cards and devices that use the MTP protocol (Android phones, audio DAT,
    etc.)

--- .local/bin/desktop_programs/lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env zsh
-
-TMPBG=/tmp/screen.png
-LOCK=/tmp/lock.png
-RES=1366x768
-
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
