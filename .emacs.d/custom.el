86497b0c5c1aceae1414826ade8dc8c57269cc51
Nicola Ferru aka NFVblog <ask.nfvblog@outlook.it>
2024-06-28 00:49:08 +0200

    add emacs selected package

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,24 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company-spell company-c-headers cobol-mode pdf-tools org-modern org-contrib company auctex)))
+   '(grandshell-theme
+     company
+     company-spell
+     company-c-headers
+     auctex
+     powerline
+     cobol-mode
+     pdf-tools
+     org-modern
+     org-contrib
+     elfeed-org
+     org-roam
+     org-roam-ui
+     org-tree-slade
+     octave
+     neotree
+     vterm
+     vterm-toggle)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
