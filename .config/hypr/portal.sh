942ad1d6dcaded01e50a30e200fb4ddec9233bd2
nfvblog <ask dot nfvblog at outlook dot it>
2024-09-21 11:58:39 +0200

    update

--- .config/hypr/portal.sh
+++ .config/hypr/portal.sh
@@ -2,9 +2,12 @@
 sleep 1
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-gtk
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
+
+# reboot pipewire
+gentoo-pipewire-launcher restart &
+sleep 1
+
 logger 'killed all xdg-desktop'
 sleep 1
 /usr/libexec/xdg-desktop-portal-gtk &
