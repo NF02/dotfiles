50b008b9ef9402c2a920bd1b5b65b6a055b32633
Nicola Ferru Aka NFVblog <nicoferru190@gmail.com>
2026-05-07 21:05:24 +0200

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,17 +5,11 @@
 (add-hook 'emacs-startup-hook
           (lambda () (setq gc-cons-threshold (* 2 1024 1024))))
 
-;; 2. UI Immediata
-(setq inhibit-startup-screen t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-;; 3. Definizioni Percorsi (Fondamentale farlo PRIMA dei caricamenti)
+;; 2. Definizioni Percorsi (Fondamentale farlo PRIMA dei caricamenti)
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;; 4. Gestione Pacchetti
+;; 3. Gestione Pacchetti
 (require 'package)
 (setq package-archives '(("melpa"  . "https://melpa.org/packages/")
                          ("elpa"   . "https://elpa.gnu.org/packages/")
@@ -28,7 +22,7 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; 5. Caricamento Moduli (Con protezione contro i crash)
+;; 4. Caricamento Moduli (Con protezione contro i crash)
 (defun my/safe-load (module)
   "Carica un modulo solo se esiste, senza bloccare l'avvio."
   (condition-case err
@@ -45,13 +39,15 @@
 (my/safe-load 'gui)
 (my/safe-load 'dev)
 (my/safe-load 'web)
-(my/safe-load 'latex)
+(my/safe-load 'latex-conf)
 (my/safe-load 'typst)
 (my/safe-load 'org-conf)
 (my/safe-load 'rss)
+(my/safe-load 'git)
+(my/safe-load 'llm)
+(my/safe-load 'lilypond)
 
-
-;; 6. Modernizzazione Minibuffer (Vertico)
+;; 5. Modernizzazione Minibuffer (Vertico)
 (use-package vertico
   :init (vertico-mode))
 
@@ -59,11 +55,11 @@
   :ensure t
   :init (marginalia-mode)) ; Aggiunge descrizioni ai comandi M-x
 
-;; 7. Fine: Carica le personalizzazioni salvate dalla UI
+;; 6. Fine: Carica le personalizzazioni salvate dalla UI
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; 8. Ispell
+;; 7. Ispell
 (use-package ispell
   :if (executable-find "hunspell")
   :config
