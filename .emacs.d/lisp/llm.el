3e5b73d2a966d1995bd62f157e0d0a77f8859acf
Nicola Ferru Aka NFVblog <nicoferru190@gmail.com>
2026-05-17 12:23:36 +0200

    LLM client configuration file

---
+++ .emacs.d/lisp/llm.el
@@ -0,0 +1,21 @@
+;;; llm.el --- Configurazione GPTel + Ollama
+
+(use-package gptel
+  :ensure t
+  :defer t
+  :config
+  (setq gptel-model 'qwen2.5-coder:1.5b
+        gptel-backend (gptel-make-ollama "Ollama"
+                         :host "localhost:11434"
+                         :models '(qwen2.5-coder:1.5b)))
+  :bind (("C-c g" . gptel-send)
+         ("C-c M-g" . gptel-menu)))
+
+(use-package gptel-agent
+  :ensure t
+  :after gptel
+  :config
+  (setq gptel-agent-default-agent 'programmer
+        gptel-agent-programmer-default-model "qwen2.5-coder:1.5b"))
+
+(provide 'llm)
